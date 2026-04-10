c33ad9c8a6674d62d0643f3e0a4e534e6d070ffe
andywilkinshmpps <andrew.wilkins@justice.gov.uk>
2026-04-10 14:17:50 +0100

    Add script to create secrets (#3)
    * Update values.yaml
    
    disable secret that is stopping the pod from starting
    
    * Script to create secrets
    
    Add a node.js script to create and update values for required secrets
    
    * Fix linter errors in script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
-export default hmppsConfig()
+export default [
+  ...hmppsConfig(),
+  {
+    files: ['scripts/**/*.mjs'],
+    languageOptions: {
+      sourceType: 'module',
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+    rules: {
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-continue': 'off',
+      'prefer-destructuring': 'off',
+      'no-shadow': 'off',
+      'prettier/prettier': 'off',
+    },
+  },
+]
